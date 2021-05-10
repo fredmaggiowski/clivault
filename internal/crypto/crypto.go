992a8d00cd5e3d2cb8c5df520401b91b6098c3bb
Federico Maggi <federico.maggi@mia-platform.eu>
2021-05-10 22:44:14 +0200

    refactor: nonce fn

--- internal/crypto/crypto.go
+++ internal/crypto/crypto.go
@@ -11,6 +11,14 @@ type AESCryptoCipher struct {
 	key []byte
 }
 
+func makeNonce(size int) ([]byte, error) {
+	nonce := make([]byte, size)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	return nonce, nil
+}
+
 func NewAESCryptoCipher(key []byte) *AESCryptoCipher {
 	return &AESCryptoCipher{key}
 }
@@ -26,14 +34,12 @@ func (aesc *AESCryptoCipher) Encrypt(plaintext []byte, username string) ([]byte,
 		return nil, err
 	}
 
-	nonce := make([]byte, aesgcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+	nonce, err := makeNonce(aesgcm.NonceSize())
+	if err != nil {
 		return nil, err
 	}
 
-	ciphertext := aesgcm.Seal(nonce, nonce, []byte(plaintext), []byte(username))
-	// fmt.Printf("DATA USED IN ENC %v %s %v\n", nonce, username, ciphertext)
-	return ciphertext, nil
+	return aesgcm.Seal(nonce, nonce, []byte(plaintext), []byte(username)), nil
 }
 
 func (aesc *AESCryptoCipher) Decrypt(ciphertext []byte, username string) ([]byte, error) {
@@ -54,7 +60,6 @@ func (aesc *AESCryptoCipher) Decrypt(ciphertext []byte, username string) ([]byte
 
 	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
 
-	// fmt.Printf("DATA USED IN DEC %v %s\n", nonce, username)
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, []byte(username))
 	if err != nil {
 		return nil, err
